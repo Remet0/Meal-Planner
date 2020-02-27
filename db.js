c0f88798d6ccf0615c2fc4bf5e6e3c26efa59e66
Remeto <remetojohn@gmail.com>
2020-02-27 09:32:45 -0800

    fixed database connection issues

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: 'MealPlanner'
+  database: process.env.DB_NAME
 });
 
 connection.connect(err => {
