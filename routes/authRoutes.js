4287cb1a68b7f9de61529bb2eaf3ee8f349441cc
Remeto <remetojohn@gmail.com>
2020-03-03 14:34:23 -0800

    updated register route, need to fix deconstruction and test route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,25 @@
 const express = require('express');
+let createToken = require('../util/createToken');
+const User = require('../models/userModel');
 const authRoutes = express.Router();
 
-authRoutes.put('/register', (req, res) => {
+authRoutes.post('/user', async (req, res) => {
+  let { username, password, admin } = req.body;
+
+  username = username.trim().toLowerCase();
+  password = password.trim();
+
+  try {
+    const user = await User.create({
+      username: username,
+      password: password,
+      admin: admin
+    });
+    const token = await createToken(user);
+    res.status(200).send({ user, token });
+  } catch {
+    res.status(400).send('error making user');
+  }
   res.status(200).send('req working');
 });
 
