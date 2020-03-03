0f64e73cae4bde52da2b3c4221aa14c4f5453267
Remeto <remetojohn@gmail.com>
2020-03-03 09:51:39 -0800

    finishing up login util

--- util/loginUser.js
+++ util/loginUser.js
@@ -7,6 +7,32 @@ module.exports = loginUser = async (req, res) => {
   password = password.trim();
 
   if (!username) {
-    res.status(400).send('no username found');
+    res.status(400).send('Username is missing');
+  }
+  if (!password) {
+    res.status(400).send('Password is missing');
+  }
+
+  try {
+    const user = await connection.query(
+      `SELECT * FROM Users WHERE username = ${username}`,
+      (err, rows) => {
+        if (err) throw err;
+
+        return rows;
+      }
+    );
+
+    const match = false;
+    if (username === user.username) {
+      match = true;
+    }
+    if (!match) {
+      return res.status(400).send('username or password are incorrect');
+    }
+
+    req.tokenString = CreateToken(user);
+  } catch {
+    return res.status(500).send('Error processing request');
   }
 };
