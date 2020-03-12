84da8df21e65c3160c5dab42683fc8eeacca83b3
Remeto <remetojohn@gmail.com>
2020-03-12 09:32:42 -0700

    created basic nav bar. still need to finish styling

---
+++ client/src/Components/Nav.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NavBar = () => {
+  return (
+    <NavWrapper>
+      <Header>Meal Planner</Header>
+      <List>
+        <ListLink>Home</ListLink>
+        <ListLink>Login</ListLink>
+      </List>
+      <NavAccent></NavAccent>
+    </NavWrapper>
+  );
+};
+
+export default NavBar;
+
+const NavWrapper = styled.section`
+  display: flex;
+  position: relative;
+  height: 50px;
+  background-color: rgb(31, 156, 252);
+`;
+
+const NavAccent = styled.hr`
+  margin: 0;
+  position: absolute;
+  bottom: 0;
+  border: 1px solid rgb(45, 115, 31);
+  width: 100vw;
+  height: 0;
+  box-shadow: 0px 1px 2px grey;
+`;
+
+const Header = styled.h1`
+  position: absolute;
+  left: 10px;
+  top: 0px;
+  margin-top: 10px;
+`;
+
+const List = styled.ul`
+  list-style-type: none;
+  position: absolute;
+  right: 2em;
+`;
+
+const ListLink = styled.li`
+  display: inline;
+  margin-right: 2em;
+`;
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import { NavBar } from '../Components/index';
 
 const HomePage = () => {
   return (
     <>
-      <p>Home Page</p>
+      <Wrapper>
+        <NavBar></NavBar>
+      </Wrapper>
     </>
   );
 };
 
 export default HomePage;
+
+const Wrapper = styled.section`
+  background-color: grey;
+  height: 100%;
+  width: 100%;
+`;
