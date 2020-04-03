366ae6c52364d80330c1b4b9984632e3775136eb
Remeto <remetojohn@gmail.com>
2020-04-03 12:36:42 -0700

    made simple modal wrapper

--- client/src/Components/CreateUser.jsx
+++ client/src/Components/CreateUser.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const CreateUser = () => {
   const [show, setShow] = useState(false);
@@ -16,10 +17,21 @@ const CreateUser = () => {
 
   return (
     <>
-      <p>Modal stuff goes here</p>
-      <button onClick={handleClose}> Close modal</button>
+      <ModalWrap>
+        <button onClick={handleClose}> Close modal</button>
+      </ModalWrap>
     </>
   );
 };
 
 export default CreateUser;
+
+const ModalWrap = styled.section`
+  margin-top: 50px;
+  margin-left: 50px;
+  height: 500px;
+  width: 90vw;
+  background-color: white;
+  box-shadow: 5px 5px 15px;
+  z-index: 999;
+`;
